d4c7ea81d22b71c9e2ed2c932479c621a606bae8
Daniel <caoxiaopeng@raycloud.com>
2018-02-05 12:24:50 +0800

    添加timeout处理

--- app/extend/context.js
+++ app/extend/context.js
@@ -2,7 +2,10 @@
 
 module.exports = {
     dubbo(className, method, ...javaParams) {
-    const data = this.app.dubboClinent.request(className, method, javaParams);
-    return data;
-},
+        const timeout =  this.app.dubboClinent.dubboAgent.dependencies[className].dubboTimeout || this.app.dubboClinent.dubboTimeout || 30000;
+        return Promise.race([
+            this.app.dubboClinent.request(className, method, javaParams),
+            new Promise((_, reject) => setTimeout(() => reject(new Error('timeout')), timeout))
+        ]);
+    },
 };
